6416007d731f8695852bbd83a2496c92c20abc67
fartb <fartbreather228@gmail.com>
2025-06-24 01:30:37 +0300

    задание 2: добавлен класс продукта

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+using static System.Console;
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Cost { get; set; }
+
+    public Product(string name, string cost)
+    {
+        Name = name;
+        Cost = cost;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}\t-\t{Cost}";
+    }
+}
+
+//using (XmlTextWriter writer = new XmlTextWriter("orders.xml", System.Text.Encoding.UTF8))
+//{
+//    writer.Formatting = Formatting.Indented;
